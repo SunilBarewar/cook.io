86c144fbce41771d1ab710f33e19c64fa8786559
sunil barewar <sunilvbarewar@gmail.com>
2023-07-18 00:12:55 +0530

    tabs and tab panel navigation

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,16 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
 
 "use strict";
+
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements  nodeList
+ * @param {String} eventType Event type string
+ * @param {Function} callback  callback function
+ */
+
+window.addEventOnElements = ($elements, eventType, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback);
+    }
+}
